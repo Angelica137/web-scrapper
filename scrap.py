c144b4e65b61722a409a16237b186de758fde9bb
Angelica <Angelica137@users.noreply.github.com>
2020-09-13 21:07:58 +0100

    remove obsolete

--- scrap.py
+++ scrap.py
@@ -10,7 +10,6 @@ results = soup.find(id='ResultsContainer')
 
 job_elems = results.find_all('section', class_='card-content')
 
-print('Printing job elem')
 for job_elem in job_elems:
     title_elem = job_elem.find('h2', class_='title')
     company_elem = job_elem.find('div', class_='company')
