333b610fd1f4e97cf445167818f9e775a631d15b
Angelica <Angelica137@users.noreply.github.com>
2020-09-13 19:51:44 +0100

    get card contents

--- scrap.py
+++ scrap.py
@@ -9,3 +9,10 @@ soup = BeautifulSoup(page.content, 'html.parser')
 results = soup.find(id='ResultsContainer')
 
 print(results.prettify())
+
+job_elems = results.find_all('section', class_='card-content')
+
+print('Printing job elem')
+for job_elem in job_elems:
+    print(job_elem, end='\n'*2)
+
